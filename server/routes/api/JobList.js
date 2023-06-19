2c340c6b9605a059101732f4dd66835aa86ca0ac
jbrodric <john.brodrick@gmail.com>
2023-06-19 17:07:09 -0400

    Routes Upd1

--- server/routes/api/JobList.js
+++ server/routes/api/JobList.js
@@ -1,24 +1,24 @@
 const { Router } = require("express");
-const Todo = require("../../models/JobPosting");
+const JobPosting = require("../../models/JobPosting");
 
 const router = Router();
 
 router.get("/", async (req, res) => {
   try {
-    const todoList = await Todo.find();
-    if (!todoList) throw new Error("No Todo List found");
-    res.status(200).json(todoList);
+    const jobList = await JobPosting.find();
+    if (!jobList) throw new Error("No Jobs found");
+    res.status(200).json(jobList);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
 
 router.post("/", async (req, res) => {
-  const newTodo = new Todo(req.body);
+  const newJob = new JobPosting(req.body);
   try {
-    const todo = await newTodo.save();
-    if (!todo) throw new Error("Something went wrong saving the Todo");
-    res.status(200).json(todo);
+    const job = await newJob.save();
+    if (!job) throw new Error("Something went wrong saving the job");
+    res.status(200).json(job);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -27,7 +27,7 @@ router.post("/", async (req, res) => {
 router.delete("/:id", async (req, res) => {
   const { id } = req.params;
   try {
-    const removed = await Todo.findByIdAndDelete(id);
+    const removed = await JobPosting.findByIdAndDelete(id);
     if (!removed) throw Error("Something went wrong ");
     res.status(200).json(removed);
   } catch (error) {
