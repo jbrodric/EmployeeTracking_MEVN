5460cb8e0a52b29b85c2233234ade8a76d05bb69
jbrodric <john.brodrick@gmail.com>
2023-06-26 13:58:51 -0400

    Initial Take on Header

--- client/empts_client/src/components/RecordHeader.js
+++ client/empts_client/src/components/RecordHeader.js
@@ -1,20 +1,18 @@
 import { Form } from "react-router-dom";
-import { Paper, Button } from "@mui/material";
+import { Paper, Button, Typography, Box } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import WorkIcon from "@mui/icons-material/Work";
+import Grid from "@mui/material/Unstable_Grid2";
 
-const StyledPaper = styled((props) => (
+const HeaderPaper = styled((props) => (
   <Paper
     sx={{
       fontSize: "0.9rem",
       minHeight: "64px",
       margin: "1% 3% 1% 3%",
       padding: "1%",
-      display: "flex",
-      justifyContent: "right",
-      alignItems: "center",
     }}
     {...props}
   />
@@ -26,38 +24,88 @@ const StyledPaper = styled((props) => (
 
 export default function RecordHeader() {
   return (
-    <StyledPaper variant="elevation" elevation={8}>
-      <WorkIcon />
-      <Form action="edit">
-        <Button
-          variant="outlined"
-          type="submit"
-          sx={{ margin: "10px", backgroundColor: "white" }}
-          startIcon={<EditIcon />}
-        >
-          Edit
-        </Button>
-      </Form>
-      <Form
-        method="post"
-        action="destroy"
-        onSubmit={(event) => {
-          if (
-            !window.confirm("Please confirm you want to delete this record.")
-          ) {
-            event.preventDefault();
-          }
-        }}
+    <HeaderPaper variant="elevation" elevation={8}>
+      <Box
+        id="header-container"
+        sx={{ display: "flex", flexFlow: "row", height: "100%" }}
       >
-        <Button
-          variant="outlined"
-          type="submit"
-          sx={{ margin: "10px", backgroundColor: "white" }}
-          startIcon={<DeleteIcon />}
+        <Box
+          id="header-icon"
+          className="center-content"
+          sx={{ flex: "0 1 auto", marginRight: "10px" }}
         >
-          Delete
-        </Button>
-      </Form>
-    </StyledPaper>
+          <WorkIcon
+            fontSize="large"
+            sx={{
+              color: "rgb(255,255,255)",
+              backgroundColor: "rgb(25, 118, 210)",
+              borderRadius: "5px",
+              padding: "6px",
+            }}
+          />
+        </Box>
+        <Box id="header-grid" sx={{ flex: "1 1 auto" }}>
+          <Grid container rowSpacing={0} columnSpacing={0}>
+            <Grid container xs={9}>
+              <Grid xs={12}>
+                <Typography sx={{ margin: "0px" }}>Job</Typography>
+              </Grid>
+              <Grid xs={12}>
+                <Typography
+                  variant="h6"
+                  component="div"
+                  noWrap={true}
+                  width="600px"
+                  sx={{ fontWeight: "bold", margin: "0px" }}
+                >
+                  Test JobTest JobTest JobTest JobTest JobTest JobTest Job
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid
+              xs={3}
+              sx={{
+                display: "flex",
+                justifyContent: "right",
+                alignItems: "center",
+              }}
+            >
+              <Form action="edit">
+                <Button
+                  variant="outlined"
+                  type="submit"
+                  sx={{ margin: "10px", backgroundColor: "white" }}
+                  startIcon={<EditIcon />}
+                >
+                  Edit
+                </Button>
+              </Form>
+              <Form
+                method="post"
+                action="destroy"
+                onSubmit={(event) => {
+                  if (
+                    !window.confirm(
+                      "Please confirm you want to delete this record."
+                    )
+                  ) {
+                    event.preventDefault();
+                  }
+                }}
+              >
+                <Button
+                  variant="outlined"
+                  type="submit"
+                  sx={{ margin: "10px", backgroundColor: "white" }}
+                  startIcon={<DeleteIcon />}
+                >
+                  Delete
+                </Button>
+              </Form>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </HeaderPaper>
   );
 }
